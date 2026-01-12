d1b55039367aa67075b6f3065ccc3e815455374a
Ted Choi <rhkdtn2056@gmail.com>
2026-01-13 08:21:20 +0900

    adding more pages using the nested forloop

--- pdf_gen/main.py
+++ pdf_gen/main.py
@@ -8,12 +8,16 @@ pdf = FPDF(orientation='P', unit="mm", format="A4")
 df = pd.read_csv('topics.csv')
 
 # syntax within padas for going through each row of the data in the csv file
-for index, topic in df.iterrows():
+for index, row in df.iterrows():
     pdf.add_page() 
     pdf.set_font(family="Times", style="B", size=12)
     pdf.cell(w=0, h=12, txt="hello there!", align="L", ln=1)
     # pdf.line(x1, y1, x2, y2)
     pdf.line(10, 22, 200, 22)
+
+    for i in range(row['Pages'] - 1):
+        pdf.add_page()
+
     
 
 pdf.output("output.pdf")
