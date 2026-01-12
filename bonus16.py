e30b99e160b02d83d8d7b38dfd6ab166637ae0c2
Ted Choi <rhkdtn2056@gmail.com>
2026-01-12 10:16:06 +0900

    completion of zip file compressor app

--- bonus16.py
+++ bonus16.py
@@ -1,17 +1,26 @@
 import FreeSimpleGUI as fsg
-
+from zip_creator import make_archive
 
 label1 = fsg.Text('select files to compress')
 input1 = fsg.Input()
-choose_button1 = fsg.FileBrowse('choose')
+choose_button1 = fsg.FileBrowse('choose', key="files")
 
 
 label2 = fsg.Text('select destination folder')
 input2 = fsg.Input()
-choose_button2 = fsg.FolderBrowse('choose')
+choose_button2 = fsg.FolderBrowse('choose', key="folder")
+
 
 compress = fsg.Button('compress')
+out_msg = fsg.Text(key='output')
 
-window = fsg.Window('file compressor', layout=[[label1, input1, choose_button1], [label2, input2, choose_button2], [compress]])
-window.read()
+window = fsg.Window('file compressor', layout=[[label1, input1, choose_button1], [label2, input2, choose_button2], [compress, out_msg]])
+
+while True: 
+    event, values = window.read()
+    print(event, values)
+    filepaths = values['files'].split(';')
+    folder = values['folder']
+    make_archive(filepaths, folder)
+    window['output'].update(value='completed!')
 window.close()
