ca4e06414985e85bdd7663c150b6a0858fe287d1
Ted Choi <rhkdtn2056@gmail.com>
2026-01-14 08:33:26 +0900

    change the subject of the email and adding more content and dynamic topics

--- Email_API/main.py
+++ Email_API/main.py
@@ -1,17 +1,26 @@
 import requests
-import send_email
+# import send_email
+
+# make it more dynamic for various topics
+topic = 'tesla'
 
 api_key = "9a23c1c29bda4520b3f536b36ebecf21"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2025-12-13&sortBy=publishedAt&apiKey=9a23c1c29bda4520b3f536b36ebecf21"
+url = "https://newsapi.org/v2/everything?" \
+        f"q={topic}&" \
+        "from=2025-12-13&" \
+        "sortBy=publishedAt&" \
+        "apiKey=9a23c1c29bda4520b3f536b36ebecf21"
 
 request = requests.get(url)
 
 content = request.json()
 
 body=''
-for article in content['article']:
+for article in content['articles']:
     if article['title'] is not None:
-        body = body + article['title'] + '\n' + article['description'] + 2 * '\n'
+        "Subject: Today's News"
+        body = body + article['title'] + '\n' \
+    + '\n' + article['description'] + '\n' + article['url'] + 2 * '\n'
 
 body = body.encode('utf-8')
 send_email(message=body)
