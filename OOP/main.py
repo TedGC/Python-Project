fd9a5ce1517f189f6f678f4e352ab0c8a6ddfb3f
Ted Choi <rhkdtn2056@gmail.com>
2026-01-17 22:03:14 +0900

    self, iniit method and creating class

--- OOP/main.py
+++ OOP/main.py
@@ -12,7 +12,7 @@ URL = "http://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-connection = sqlite3.connect("data.db")
+
 
 
 class FirstClass:
@@ -28,50 +28,57 @@ class FirstClass:
         value = extractor.extract(source)["tours"]
         return value
 
+class Email:
+    def send(self, message):
+        host = "smtp.gmail.com"
+        port = 465
 
-def send_email(message):
-    host = "smtp.gmail.com"
-    port = 465
+        username = "app8flask@gmail.com"
+        password = "qyciukmocfaiarse"
 
-    username = "app8flask@gmail.com"
-    password = "qyciukmocfaiarse"
+        receiver = "app8flask@gmail.com"
+        context = ssl.create_default_context()
 
-    receiver = "app8flask@gmail.com"
-    context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(host, port, context=context) as server:
+            server.login(username, password)
+            server.sendmail(username, receiver, message)
+        print("Email was sent!")
 
-    with smtplib.SMTP_SSL(host, port, context=context) as server:
-        server.login(username, password)
-        server.sendmail(username, receiver, message)
-    print("Email was sent!")
+class DataBase:
 
+    def __init__(self):
+        self.connection = sqlite3.connect("data.db")
 
-def store(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
-    connection.commit()
+    def store(self, extracted):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        cursor = self.connection.cursor()
+        cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+        self.connection.commit()
 
-def read(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    band, city, date = row
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
-    rows = cursor.fetchall()
-    print(rows)
-    return rows
+    def read(self, extracted):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        band, city, date = row
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+        rows = cursor.fetchall()
+        print(rows)
+        return rows
 
 
 if __name__ == "__main__":
     while True:
-        scraped = scrape(URL)
-        extracted = extract(scraped)
+        event = FirstClass()
+        scraped = event.scrape(URL)
+        extracted = event.extract(scraped)
         print(extracted)
 
         if extracted != "No upcoming tours":
-            row = read(extracted)
+            database = DataBase()
+            row = database.read(extracted)
             if not row:
-                store(extracted)
-                send_email(message="Hey, new event was found!")
+                database.store(extracted)
+                email = Email()
+                email.send(message="Hey, new event was found!")
         time.sleep(2)
