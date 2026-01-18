9b5024416468417eb149624d60bd7f893c65caac
Ted Choi <rhkdtn2056@gmail.com>
2026-01-18 14:33:20 +0900

    creating new classes and connecting them through the operation

---
+++ OOP2/main.py
@@ -0,0 +1,51 @@
+# class User:
+#     def view_hotels(self): 
+#         pass
+
+import pandas as pd
+
+df = pd.read_csv('hotels.csv', dtype={'id':str})
+
+
+class Hotel: 
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
+        pass
+
+    def book(self):
+        df.loc[df['id'] == self.hotel_id,'available'] = 'no'
+        df.to_csv('hotels.csv', index=False)
+
+    def available(self):
+        availability = df.loc[df['id'] == self.hotel_id,'available'].squeeze()
+        if  availability == 'yes':
+            return True
+        else: 
+            return False
+    
+
+
+class ReservationTicket:
+    def __init__(self, customer_name, hotel_object):
+        self.customer_name = customer_name
+
+    def generate(self): 
+        content = f"""
+        Thank you for reservation!
+        here is your booking information
+        Name :{self.customer_name}"""
+    
+
+print(df)
+
+hotel_id = input('enter the id of the hotel: ')
+hotel = Hotel(hotel_id)
+
+if hotel.available():
+    hotel.book()
+    name = input('enter your name: ')
+    reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
+    print(reservation_ticket.generate())
+
+else: 
+    print('hotel is not available at the moment')
