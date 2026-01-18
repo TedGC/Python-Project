35d40bb5ec8c2852a27ab2d5cf9bc9c97c09ed27
Ted Choi <rhkdtn2056@gmail.com>
2026-01-18 14:47:49 +0900

    OOP for hotel reservation

--- OOP2/main.py
+++ OOP2/main.py
@@ -10,7 +10,7 @@ df = pd.read_csv('hotels.csv', dtype={'id':str})
 class Hotel: 
     def __init__(self, hotel_id):
         self.hotel_id = hotel_id
-        pass
+        self.name = df.loc[df['id'] == self.hotel_id,'name'].squeeze()
 
     def book(self):
         df.loc[df['id'] == self.hotel_id,'available'] = 'no'
@@ -28,12 +28,15 @@ class Hotel:
 class ReservationTicket:
     def __init__(self, customer_name, hotel_object):
         self.customer_name = customer_name
+        self.hotel = hotel_object
 
     def generate(self): 
         content = f"""
         Thank you for reservation!
         here is your booking information
-        Name :{self.customer_name}"""
+        Name :{self.customer_name}
+        Hotel Name: {self.hotel.name}"""
+        return content
     
 
 print(df)
