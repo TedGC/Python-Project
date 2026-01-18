d4f02b4a071a88e7dc74513bc8e905c59ca51f00
Ted Choi <rhkdtn2056@gmail.com>
2026-01-18 20:18:02 +0900

    EXAMPLE

---
+++ sql_DB/example.py
@@ -0,0 +1,14 @@
+from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, \
+QLineEdit
+
+class AgeCalculator(QWidget):
+    def __init__(self):
+        grid = QGridLayout()
+
+        name_label = QLabel("Name: ")
+        name_line_edit = QLineEdit()
+    
+    
+
+
+age_calculator = AgeCalculator() 
